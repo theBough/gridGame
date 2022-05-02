51c2e513540e6bc52992ea2d9fb81f2b1953dd29
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-02 08:57:03 -0500

    Create square.js

---
+++ square.js
@@ -0,0 +1,12 @@
+let s;
+function createSquares(){
+  s = new Clickable(200,200);
+  s.resize(50,50)
+  s.color = " red"
+  s.text = "W"
+  s.cornerRadius = 1
+  //try text color
+  //try stroke
+  //try w
+  
+}//end CreateSquare
