2ad3081b1da4de81ca78d7240b3525619bc59618
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-12 10:32:50 -0500

    Update stick.js

--- stick.js
+++ stick.js
@@ -3,11 +3,14 @@ function createStick(){
   st = new Clickable(20,400);
   st.resize(30,150);
   st.cornerRadius = 1;
-  st.text = "";
+  
   st.onPress = function(){
     st.isClicked = true;
   }//end onPress
   st.onRelease = function(){
     st.isClicked = false;
+    st.text = st.x;
+    st.x = Math.floor(st.x /30) *30
+    st.y = Math.floor(st.y /30) *30
   }//end onRelease
 }
