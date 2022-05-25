c01ba4665daad3e6045c47c4fd5c9c41b855be4c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-25 11:03:27 -0500

    Update cube.js

--- cube.js
+++ cube.js
@@ -6,13 +6,34 @@ function createCube(){
   c.color = "blue";
   c.onPress = function(){
     c.isClicked = true;
-  }
+  }//end onPress
   c.onRelease = function(){
     c.isClicked = false;
     c.text = "";
     c.x = Math.floor(st.x / 30) * 30;
     c.y = Math.floor(st.y / 30) * 30;
-    findSquare();
-  }
+    findCube();
+  }//end on Release
   
-}
+}//end createCube
+
+function findCube() {
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      if (s[i][j].x == c.x && s[i][j].y == c.y) {
+        //we found the square where the stick was dropped.
+        s[i][j].color ="yellow";
+        s[i+1][j].color ="yellow";
+        s[i+2][j].color ="yellow";
+        s[i][j+1].color ="yellow";
+        s[i][j+2].color ="yellow";
+        s[i+1][j+1].color ="yellow";
+        s[i+1][j+2].color ="yellow";
+        s[i+2][j+1].color = "yellow";
+        s[i+2][j+2].color ="yellow";
+        
+        c.locate(20,400);
+      } //if
+    } //j loop
+  } //i loop
+} //find square
